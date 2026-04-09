d352937af26d10f3344ad15db77231a191b544cd
Your Salespoint <info@yoursalespoint.nl>
2026-04-09 20:34:05 +0200

    Add GTM-5CCGHTW9

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="nl">
-      <GoogleTagManager gtmId="GTM-XXXXXXX" />
+      <GoogleTagManager gtmId="GTM-5CCGHTW9" />
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
